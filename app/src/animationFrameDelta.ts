e513e6873f8e70ed2cdd119923c3475aaba7e774
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-08-30 17:12:48 +0200

    update build infrastructure

--- app/src/animationFrameDelta.ts
+++ app/src/animationFrameDelta.ts
@@ -1,2 +1,7 @@
-export * from "./browser"
+
 export * from "./envAgnostic"
+
+
+
+import { stats, nextFrame, unsubscribe, subscribe } from "./browser"
+export { stats, nextFrame, unsubscribe, subscribe }
---
+++ repl/src/repl.ts
@@ -0,0 +1,20 @@
+// import * as animationFrameDelta from "../../app/src/animationFrameDelta"
+
+// let time = 0
+// const e = animationFrameDelta.subscribe((e, w, delta) => {
+//   time += delta
+//   console.log(time)
+// })
+
+// setTimeout(() => {
+//   e.cancel()
+//   setTimeout(() => {
+//     e.resume()
+//   }, 1000)
+// }, 1000)
+
+
+
+import { now } from "../../app/src/animationFrameDelta"
+
+console.log(now())
